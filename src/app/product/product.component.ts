6e53dd0007c8a8507fed6225d8b2fff64725a01d
Alpeshkumar Prajapati <a.prajapati@metrosystems-external.net>
2018-09-17 15:06:54 +0530

    Added bootstrap, ng-bootstrap module, UI for product list page

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../core/services/product.service';
-
+import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -9,10 +9,12 @@ import { ProductService } from '../core/services/product.service';
 export class ProductComponent implements OnInit {
 
   private  products:  Array<object> = [];
+  public imageEndpoint: string = environment.imageEndpoint;
   
   constructor( private _productService: ProductService ) { }
 
   ngOnInit() {
+    console.log('host: ..', this.imageEndpoint )
     this.getProducts();
   }
 
