b188271f6d809734b6eb6180eecdd276a7149ad7
Alpeshkumar Prajapati <a.prajapati@metrosystems-external.net>
2018-09-18 19:53:54 +0530

    Added functionality to remove product from wishlist with alert

--- src/app/shared/shared-bootstrap.module.ts
+++ src/app/shared/shared-bootstrap.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { AlertModule } from 'ngx-bootstrap/alert';
 
 @NgModule({  
-    imports: [BsDropdownModule.forRoot(), PaginationModule.forRoot(), AccordionModule.forRoot()],  
-    exports: [BsDropdownModule, PaginationModule, AccordionModule],  
+    imports: [BsDropdownModule.forRoot(), PaginationModule.forRoot(), AccordionModule.forRoot(), AlertModule.forRoot()],  
+    exports: [BsDropdownModule, PaginationModule, AccordionModule, AlertModule],  
     declarations: [],  
     providers: []  
 })  
--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishlistComponent implements OnInit {
   public wishlistData: Array<Object> = [];
+  public showRemovedProductAlert: boolean = false;
 
   constructor() { }
 
@@ -54,6 +55,29 @@ export class WishlistComponent implements OnInit {
         "url": "/medias/sys_master/images/images/h0c/h8a/8863634948126/63033-1-2-1.jpg"
       }]
     }];
+  }
+
+  // takes product and wishlist name from which product needs to removed
+  public removeProductFromWishlist( product, wishlistname ) {
+    // Filter which wishlist product does belong to
+    let item = this.wishlistData.filter((wishlist:any) => wishlist.name === wishlistname );
+    this.removeProduct( item[0], product.code );
+  }
+
+  // Remove  specific product form products array using product' code property
+  public removeProduct( item, productCode ) {
+    let productArray = item.products;
+    for (var i =0; i < productArray.length; i++) {
+      if (productArray[i].code === productCode ) {
+        productArray.splice(i,1);
+        this.showRemovedProductAlert = true;
+        break;
+      }
+    }
+  }
+
+  public onRemovedAlertClosed() {
+    this.showRemovedProductAlert = false;
   }
 
 }
