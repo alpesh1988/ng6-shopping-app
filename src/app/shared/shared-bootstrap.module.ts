28d015ddcf1a2eba866ee78ad936b9055a9d7469
Alpeshkumar Prajapati <a.prajapati@metrosystems-external.net>
2018-09-18 17:39:43 +0530

    Added accordion for wishlist, updated environment property for prod

--- src/app/shared/shared-bootstrap.module.ts
+++ src/app/shared/shared-bootstrap.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'; 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
-  
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
 @NgModule({  
-    imports: [BsDropdownModule.forRoot(), PaginationModule.forRoot()],  
-    exports: [BsDropdownModule, PaginationModule],  
+    imports: [BsDropdownModule.forRoot(), PaginationModule.forRoot(), AccordionModule.forRoot()],  
+    exports: [BsDropdownModule, PaginationModule, AccordionModule],  
     declarations: [],  
     providers: []  
 })  
