3e9519902383ff48cbfa814919af6b868706f689
Alpeshkumar Prajapati <a.prajapati@metrosystems-external.net>
2018-09-20 19:38:45 +0530

    Added functional test case of searching product and add to wishlist in nightwatch

--- e2e-nightwatch/pages/products.js
+++ e2e-nightwatch/pages/products.js
@@ -5,6 +5,21 @@ module.exports = {
       },
       singleProductContainer: {
         selector: '.alp-single-product-container'
+      },
+      singleProductwishlistBtn: {
+        selector: '.alp-single-product-wishlist'
+      },
+      createWishlistInputBox: {
+          selector: '#alp-create-wishlist-input' 
+      },
+      createWishlistButton: {
+        selector: '#alp-create-wishlist-button' 
+      },
+      addToWishlistBtn: {
+        selector: '#alp-add-to-wishlist-btn'
+      },
+      addedProductAlert: {
+        selector: '.alp-added-alert'
       }
     }
   };
--- e2e-nightwatch/tests/products.js
+++ e2e-nightwatch/tests/products.js
@@ -1,5 +1,7 @@
-let smallTimeDuration = 5000,
-  mediumTimeDuration = 10000;
+let extraSmallTimeDuration = 3000,
+  smallTimeDuration = 5000,
+  mediumTimeDuration = 10000,
+  wishlistName = 'Test Wishlist';
 
 module.exports = {
     
@@ -8,16 +10,31 @@ module.exports = {
     products = browser.page.products();
   },
 
-  'Search products with sugar filter': function( browser ) {
+  'Search products with sugar filter and add a product to wishlist': function( browser ) {
     products
       .navigate(browser.globals.urls.appURL);
     browser.pause( smallTimeDuration );
     products
       .waitForElementVisible( 'body', mediumTimeDuration )
-      .setValue( '@searchInput', 'sugar' )
+      .setValue( '@searchInput', 'sugar' );
 
-    products.expect.element( '@singleProductContainer' ).to.be.present;    
+    products.expect.element( '@singleProductContainer' ).to.be.present;
     browser.pause( smallTimeDuration );
+    
+    // click wishlist button
+    products.click( '@singleProductwishlistBtn' );
+    browser.pause( smallTimeDuration );
+
+    products.setValue( '@createWishlistInputBox', wishlistName );
+    browser.pause( extraSmallTimeDuration );
+    products.click( '@createWishlistButton' );
+    browser.pause( extraSmallTimeDuration );
+    
+    products.click( 'input[ng-reflect-name="' + wishlistName + '"]');
+    browser.pause( extraSmallTimeDuration );
+    products.click( '@addToWishlistBtn' );
+    products.expect.element( '@addedProductAlert' ).to.be.present;
+    browser.pause( extraSmallTimeDuration );
   },
 
   after: function( browser ) {
